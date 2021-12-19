0a9892e013868b3136ea3d3f409ec1022f7a0a23
Exunys <76539058+Exunys@users.noreply.github.com>
2021-12-19 17:13:52 +0100

    Automatic Repair

--- Resources/Scripts/Delete Settings - Fix Script.lua
+++ Resources/Scripts/Delete Settings - Fix Script.lua
@@ -1,5 +1,5 @@
 pcall(function()
-	local Title, Functions = game:HttpGet("https://raw.githubusercontent.com/Exunys/Wall-Hack/main/Resources/Information/Title.txt?token=ASH6JMTITPDEXQ3ENVGTYXTBX4X7U"), getgenv().WallHack.Functions
+	local Title, Functions = game:HttpGet("https://raw.githubusercontent.com/Exunys/Wall-Hack/main/Resources/Information/Title.txt"), getgenv().WallHack.Functions
 
 	if isfolder(Title) then
 		delfolder(Title)
