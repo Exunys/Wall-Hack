acc7e5194a08e25ceb7f5d6506b1a6914fd01f2b
Exunys <76539058+Exunys@users.noreply.github.com>
2022-04-01 02:01:00 +0200

    Factory Reset Settings Manually

--- Resources/Scripts/Manual Factory Reset Settings.lua
+++ Resources/Scripts/Manual Factory Reset Settings.lua
@@ -55,7 +55,7 @@ getgenv().WallHack.Crosshair = {
     CenterDotColor = "0, 255, 0",
     CenterDotSize = 1,
     CenterDotTransparency = 1,
-    CenterDotFilled = true,
+    CenterDotFilled = true
   },
 
   Parts = {
